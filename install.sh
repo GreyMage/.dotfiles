b3a7b412874be8622cbbdbb0c49f6545fee8c3d1
Eric Cutler <ecutler@cmdagency.com@dev02.developmentcmd.com>
2015-05-01 13:50:41 -0400

    Init

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Get current dir (so run this script from anywhere)
+export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Update dotfiles itself first
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+#buncha symlinks
+ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
---
+++ runcom/.bash_profile
@@ -0,0 +1,17 @@
+# .bash_profile
+
+# Get the aliases and functions
+if [ -f ~/.bashrc ]; then
+	. ~/.bashrc
+fi
+
+# User specific environment and startup programs
+
+PATH=$PATH:$HOME/.local/bin:$HOME/bin
+export PATH
+
+# Source the addons in the dotfiles source dir.
+for DOTFILE in `find ~/.dotfiles/runcom/source`
+do
+  [ -f "$DOTFILE" ] && source "$DOTFILE"
+done
