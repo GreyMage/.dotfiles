296f63a1011a1f1559c4726061627a59144ec520
Eric Cutler <ecutler@cmdagency.com@dev02.developmentcmd.com>
2015-05-01 20:59:44 -0400

    grumble

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update dotfiles itself first
-[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master && git submodule --init --recursive
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master && git submodule update --init --recursive
 
 #hookup Bash profile (includes everything in runcom/sources
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
