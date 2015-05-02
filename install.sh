d52e738291823fbb4a9ec76734e5a62f35cc3cc9
Eric Cutler <ecutler@cmdagency.com@dev02.developmentcmd.com>
2015-05-01 20:02:05 -0400

    Adding Mode (kinda)

--- install.sh
+++ install.sh
@@ -6,5 +6,12 @@ export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Update dotfiles itself first
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
-#buncha symlinks
+#hookup Bash profile (includes everything in runcom/sources
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+
+#install local instance of node (should be included as a submodule
+echo "Attempting user-install of Node.JS"
+cd $DOTFILES_DIR/node/
+./configure --prefix=~/.node
+make
+make install
