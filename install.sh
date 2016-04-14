2b420f2a4fd68f2de1e9c4079a00c70e90b9aef9
Eric Cutler <ecutler@cmdagency.com>
2016-04-14 20:44:33 +0000

    Adding Linux 64-bit Node binaries, lightens up the build requirements.

--- install.sh
+++ install.sh
@@ -6,12 +6,17 @@ export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Update dotfiles itself first
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master && git submodule update --init --recursive
 
-#hookup Bash profile (includes everything in runcom/sources
+# Create local bin directory
+mkdir -p $HOME/local/bin
+
+#hookup Profile / Configs
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
-#hookup tmux config. 
 ln -sfv "$DOTFILES_DIR/runcom/.tmux.conf" ~
-#hookup Liquidprompt config
 ln -sfv "$DOTFILES_DIR/runcom/.liquidpromptrc" ~
+
+#hookup local binaries
+ln -sfv "$DOTFILES_DIR/node-v4.4.3-linux-x64/bin/node" "$HOME/local/bin/node"
+ln -sfv "$DOTFILES_DIR/node-v4.4.3-linux-x64/bin/npm" "$HOME/local/bin/npm"
 
 echo "Attempting user-install of tmux"
 chmod +x $DOTFILES_DIR/noroottmux/tmux_local_install.sh
